daaa118acbcdc55fdb7224210bbd1e3722ac7d4b
Pooja Hiranandani <37634806+poojahira@users.noreply.github.com>
2019-01-05 17:19:58 -0500

    update requirements

--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ import numpy as np
 import h5py
 import json
 import torch
-from scipy.misc import imread, imresize
 from tqdm import tqdm
 from collections import Counter
 from random import seed, choice, sample
